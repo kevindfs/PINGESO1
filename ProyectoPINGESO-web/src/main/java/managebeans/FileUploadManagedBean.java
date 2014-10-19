a19e859fd346494051808655586e9e7d47a325a3
AndresArismendi <andres.arismendi@usach.cl>
2014-10-19 16:10:26 -0300

    agregue funcionalidad de lectura en managebean de FileUpload pom de web modificado, para las dependencias necesarias faces/readinput.xhtml [*] deben cargar las dependencias del pom de web

--- ProyectoPINGESO-web/src/main/java/managebeans/FileUploadManagedBean.java
+++ ProyectoPINGESO-web/src/main/java/managebeans/FileUploadManagedBean.java
@@ -5,21 +5,34 @@
  */
 package managebeans;
 
+import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import javax.faces.context.FacesContext;
 
-/**
- *
- * @author Kevin
- */
-@Named(value = "fileUploadManagedBean")
-@RequestScoped
+@ManagedBean
+@SessionScoped
 public class FileUploadManagedBean {
-
-    /**
-     * Creates a new instance of FileUploadManagedBean
-     */
-    public FileUploadManagedBean() {
+    UploadedFile file;
+ 
+    public UploadedFile getFile() {
+        return file;
     }
-    
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+ 
+    public void fileUploadListener(FileUploadEvent e){
+        // Get uploaded file from the FileUploadEvent
+        this.file = e.getFile();
+        // Print out the information of the file
+        System.out.println("Uploaded File Name Is :: "+file.getFileName()+" :: Uploaded File Size :: "+file.getSize());
+        // Add message
+        FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("File Uploaded Successfully"));
+    }
 }
