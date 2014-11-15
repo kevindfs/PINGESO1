f2b1f3f2149c78a910d065d21171dce067f2d2af
Italo <Italo@192.168.1.38>
2014-11-15 20:49:29 -0300

    adsasd

--- ProyectoPINGESO-ejb/src/main/java/otrasclases/Main.java
+++ ProyectoPINGESO-ejb/src/main/java/otrasclases/Main.java
@@ -17,12 +17,12 @@ public class Main {
     public static void main(String args[]) {
         // Se crea una instancia de base de datos en la carpeta mibase
         // OJO ESTA DIRECCION ES DE MI NOTEBOOK, TIENES QUE CAMBIARLA 
-        Neo4J bd = new Neo4J("C:\\Users\\alonso\\Documents\\Neo4j\\tesisbd");
+        Neo4J bd = new Neo4J("C:/Users/Italo/Desktop/PINGESO/godb");
         
         // Ejemplo de carga de la base de datos.
         // Se le entrega la ubicación del archivo como argunmento.
         int accessionUno = 7, accessionDos = 8;
-        System.out.println(bd.ancestroComunMinimo(7,4));
+        System.out.println(bd.ancestroComunMinimo(3,8150));
 //        System.out.println(bd.consulta("MATCH (a: Term {accession: 8}),(a)<-[:FATHER]-(b) RETURN b.accession;").get(1));
 
         // Se busca la distancia mínima entre los nodos 0 y 7
